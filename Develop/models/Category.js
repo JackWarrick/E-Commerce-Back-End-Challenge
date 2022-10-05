961cf1c732c1f588779955115789931f816a76a0
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-10-04 20:26:27 -0500

    category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,20 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    //ID and category_name
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+
   },
   {
     sequelize,
