3e87baa365ef3bae78566796e14660ee2910aa5e
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-10-04 22:02:40 -0500

    index.js draft

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongsTo Category
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Products belongToMany Tags (through ProductTag)
+//SOMETHING IS GOING ON HERE WITH THE PRODUCT TAG MODEL HAVING multiple associations
+
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+// Tag belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
+
+});
+
 
 module.exports = {
   Product,
