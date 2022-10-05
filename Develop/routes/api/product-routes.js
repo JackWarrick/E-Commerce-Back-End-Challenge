de24905651e700df021766b3920651f445de9f49
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-10-05 10:10:32 -0500

    product routes rough draft

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,19 +3,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
+// SHOULD WORK get all products
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({include: [{model: Category}, {model: Tag}]});
+    
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
-// get one product
-router.get('/:id', (req, res) => {
+
+
+// MIGHT NOT WORK get one product
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}]
+    });
+
+    if (!productData) {
+      res.status(404).json({
+        message: 'Oh NO! No product found with this id'
+      });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
-// create new product
+
+
+// THIS IS ALREADY DONE I THINK create new product 
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -47,7 +73,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// ALREADY DONE I THINK update product
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -89,7 +115,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+
+//THIS SHOULD WORK
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData){
+      res.status(404).json({
+        message: 'OH NO! NO product found with this id'
+      });
+      return;
+    }
+res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
